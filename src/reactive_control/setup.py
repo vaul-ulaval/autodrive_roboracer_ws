b6b3f27ac2303063a5d636066ced510e6bb1beb9
William Fecteau <william-fecteau@hotmail.com>
2025-07-11 19:41:38 +0000

    Fix reactive_control package rename

--- src/reactive_control/setup.py
+++ src/reactive_control/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'wall_follow_node = control.wall_follow_node:main'
+            'wall_follow_node = reactive_control.wall_follow_node:main'
         ],
     },
 )
