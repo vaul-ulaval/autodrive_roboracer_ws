95cceeb856ade7a5e7cb00503bb0e1c686e9a476
William Fectau <william-fecteau@hotmail.com>
2025-10-02 14:50:47 -0400

    Added seminar3 package

---
+++ src/seminar3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+package_name = 'seminar3'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='norlab',
+    maintainer_email='william-fecteau@hotmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        "console_scripts": ["wall_follow_node = seminar3.ros_node:main"],
+    },
+)
